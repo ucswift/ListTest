682c48cd41a32fd73914daefbaba1b49796d9f90
Shawn Jackson <shawn@wtdt.com>
2015-04-30 22:22:59 -0700

    Finished implementation of the CasList, copied LockList unit tests and documented CasList

--- CasList.cs
+++ CasList.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime;
 using System.Threading;
 
 namespace ListTest
@@ -19,10 +17,13 @@ namespace ListTest
 	/// In this implementaiton I marked the underlying array as volitile which is a keyword to
 	/// prevent compiler optimizations that favor single threaded access.
 	/// 
-	/// In this implenetation I'm trying 
+	/// In this implenetation I'm trying limit larger locks and instead lock for smaller atomic
+	/// operations when I can. Not all operations I could find an amotic (Interlocked) option
+	/// for so I used a lower level version of lock (Monitor.Enter/Leave) to use there. 
 	/// 
 	/// I researched this implementation by looking at ConcurrentBag and ConcurrentStack 
-	/// implementations and adapted LockList.
+	/// implementations and adapted LockList. Trying to find another solution then standard
+	/// locking implementation.
 	/// </remarks>
 	/// <typeparam name="T"></typeparam>
 	public class CasList<T> : IList<T>
@@ -34,39 +35,86 @@ namespace ListTest
 		// Size of the stored items in the array (not the arrays size)
 		private int _size;
 
+		// SyncRoot for locking
+		private Object _syncRoot;
+
 		// For performence we will increase the size of the holding array by this number
 		private const int Chunk = 8;
 		#endregion Private Vairables
 
+		public CasList()
+		{
+			_values = new Node[0];
+			_size = 0;
+		}
+
 		#region Private Methods
+		private Object SyncRoot
+		{
+			get
+			{
+				// CAS way of creating the sync root
+				if (_syncRoot == null)
+					Interlocked.CompareExchange<Object>(ref _syncRoot, new Object(), null);
+
+				return _syncRoot;
+			}
+		}
+
+		/// <summary>
+		/// Verifies the array is the needed size, if it's too small then increases
+		/// the size of value array
+		/// </summary>
+		/// <param name="requestedSize"></param>
 		private void SizeArray(int requestedSize)
 		{
 			if (_values.Length >= requestedSize)
 				return;
 
+			// Increase the array size by the Chunk size
 			int newSize = _values.Length == 0 ? Chunk : _values.Length + Chunk;
 
+			// If our new size is still smaller then  our requested size just size it to that
 			if (newSize < requestedSize)
 				newSize = requestedSize;
 
+			// If were making a size change
 			if (newSize > 0)
 			{
 				Node[] objs = new Node[newSize];
 
+				// Do we have elements to copy?
 				if (_size > 0)
 				{
 					bool wasLocked = false;
-
 					try
 					{
-						Monitor.Enter(_values, ref wasLocked);
+						Monitor.Enter(SyncRoot, ref wasLocked);
+
+						// Copy our existing array objects into the new array
 						Array.Copy(_values, 0, objs, 0, _size);
 						_values = objs;
 					}
 					finally
 					{
 						if (wasLocked)
-							Monitor.Exit(_values);
+							Monitor.Exit(SyncRoot);
+					}
+				}
+				else
+				{
+					bool wasLocked = false;
+					try
+					{
+						Monitor.Enter(SyncRoot, ref wasLocked);
+
+						// Just set to the empty array
+						_values = objs;
+					}
+					finally
+					{
+						if (wasLocked)
+							Monitor.Exit(SyncRoot);
 					}
 				}
 			}
@@ -77,19 +125,22 @@ namespace ListTest
 		public IEnumerator<T> GetEnumerator()
 		{
 			bool wasLocked = false;
-			List<T> newList = null;
+			var newList = new List<T>();
 
 			try
 			{
-				Monitor.Enter(_values, ref wasLocked);
+				Monitor.Enter(SyncRoot, ref wasLocked);
+
+				// Pull out our values into a new list
 				newList = new List<T>(_values.Select(x => x.Value));
 			}
 			finally
 			{
 				if (wasLocked)
-					Monitor.Exit(_values);
+					Monitor.Exit(SyncRoot);
 			}
 
+			// Get the enumerator 
 			return newList.GetEnumerator();
 		}
 
@@ -100,9 +151,12 @@ namespace ListTest
 
 		public void Add(T item)
 		{
+			// Use a spin wait, as it's much more performant for very quick wait's without
+			// turning over execution
 			SpinWait spinWait = new SpinWait();
 			var newValue = new Node(item);
 
+			// Somewhat dervied from a framework concurrent queue/bag implementation
 			do
 			{
 				spinWait.SpinOnce();
@@ -110,7 +164,7 @@ namespace ListTest
 				var size = Thread.VolatileRead(ref _size);
 				if (size == _values.Length)
 					SizeArray(size + 1);
-			}
+			} // CAS addition here to make adding items more performant then a lock
 			while (Interlocked.CompareExchange<Node>(ref _values[Thread.VolatileRead(ref _size)], newValue, null) != null);
 
 			Interlocked.Increment(ref _size);
@@ -118,57 +172,178 @@ namespace ListTest
 
 		public void Clear()
 		{
-			throw new NotImplementedException();
+			bool wasLocked = false;
+
+			try
+			{
+				Monitor.Enter(SyncRoot, ref wasLocked);
+
+				// Reset the array and the size
+				_values = new Node[0];
+				Interlocked.Exchange(ref _size, 0);
+			}
+			finally
+			{
+				if (wasLocked)
+					Monitor.Exit(SyncRoot);
+			}
 		}
 
 		public bool Contains(T item)
 		{
-			throw new NotImplementedException();
+			//http://stackoverflow.com/questions/13361005/c-sharp-generic-class-and-equalitycomparer
+			var comparer = EqualityComparer<T>.Default;
+
+			for (int i = 0; i < Volatile.Read(ref _size); i++)
+			{
+				var v = Volatile.Read(ref _values[i]);
+				if (comparer.Equals(v.Value, item))
+					return true;
+			}
+
+			return false;
 		}
 
 		public void CopyTo(T[] array, int arrayIndex)
 		{
-			throw new NotImplementedException();
+			bool wasLocked = false;
+
+			try
+			{
+				Monitor.Enter(SyncRoot, ref wasLocked);
+
+				for (int i = 0; i < Volatile.Read(ref _size); i++)
+				{
+					// May need to implement a deep copy here
+					array[i] = _values[i].Value;
+				}
+			}
+			finally
+			{
+				if (wasLocked)
+					Monitor.Exit(SyncRoot);
+			}
 		}
 
 		public bool Remove(T item)
 		{
-			throw new NotImplementedException();
+			// Find the index of the item
+			int index = IndexOf(item);
+
+			// If didn't find anything return
+			if (index < 0)
+				return false;
+
+			// Else remove the item at the index
+			RemoveAt(index);
+
+			return true;
 		}
 
 		public int Count
 		{
-			get { throw new NotImplementedException(); }
+			// Use Volatile.Read as it tries to 'guarentee' fressness by using a memory barrier
+			get { return Volatile.Read(ref _size); }
 		}
 
 		public bool IsReadOnly
 		{
-			get { throw new NotImplementedException(); }
+			// Do we currently have a lock, if so were ReadOnly
+			get { return Monitor.TryEnter(SyncRoot, 0); }
 		}
 
 		public int IndexOf(T item)
 		{
-			throw new NotImplementedException();
+			//http://stackoverflow.com/questions/13361005/c-sharp-generic-class-and-equalitycomparer
+			var comparer = EqualityComparer<T>.Default;
+
+			for (int i = 0; i < Volatile.Read(ref _size); i++)
+			{
+				var v = Volatile.Read(ref _values[i]);
+				if (comparer.Equals(v.Value, item))
+					return i;
+			}
+
+			return -1;
 		}
 
 		public void Insert(int index, T item)
 		{
-			throw new NotImplementedException();
+			SizeArray(_size + 1);
+
+			if (index < _size)
+			{
+				bool wasLocked = false;
+
+				try
+				{
+					Monitor.Enter(SyncRoot, ref wasLocked);
+
+					Array.Copy(_values, index, _values, index + 1, _size - index);
+					_values[index] = new Node(item);
+				}
+				finally
+				{
+					if (wasLocked)
+						Monitor.Exit(SyncRoot);
+				}
+			}
+
+			Interlocked.Increment(ref _size);
 		}
 
 		public void RemoveAt(int index)
 		{
-			throw new NotImplementedException();
+			// Decrease our array size
+			Interlocked.Decrement(ref _size);
+
+			if (index < Volatile.Read(ref _size))
+			{
+				bool wasLocked = false;
+
+				try
+				{
+					Monitor.Enter(SyncRoot, ref wasLocked);
+
+					// Copies the array into the array starting at the index we want to remove
+					Array.Copy(_values, index + 1, _values, index, Volatile.Read(ref _size) - index);
+
+					// Zero out the last value
+					_values[_size] = null;
+				}
+				finally
+				{
+					if (wasLocked)
+						Monitor.Exit(SyncRoot);
+				}
+			}
 		}
 
 		public T this[int index]
 		{
-			get { throw new NotImplementedException(); }
-			set { throw new NotImplementedException(); }
+			get
+			{
+				if (index > Volatile.Read(ref _size) || index < 0)
+					throw new IndexOutOfRangeException();
+
+				return _values[index].Value;
+			}
+			set
+			{
+				if (index > Volatile.Read(ref _size) || index < 0)
+					throw new IndexOutOfRangeException();
+
+				_values[index] = new Node(value);
+
+				Monitor.PulseAll(_values);
+			}
 		}
 		#endregion IList Implementation
 
 		#region Internal Models
+		/// <summary>
+		/// Node to allow us to use CAS on array elements
+		/// </summary>
 		internal class Node
 		{
 			public T Value { private set; get; }
---
+++ Tests/CasListTests.cs
@@ -0,0 +1,160 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ListTest.Tests
+{
+	[TestFixture]
+	public class CasListTests
+	{
+		[Test]
+		public void ListInstantiation()
+		{
+			var list = new CasList<bool>();
+
+			list.Should().NotBeNull();
+		}
+
+		[Test]
+		public void AddSingleItemToList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+
+			list.Should().NotBeNull();
+			list.Count.Should().Be(1);
+		}
+
+		[Test]
+		public void AddMultileItemsToList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(7);
+			list.Add(978);
+			list.Add(35);
+			list.Add(42);
+
+			list.Should().NotBeNull();
+			list.Count.Should().Be(5);
+		}
+
+		[Test]
+		public void ListIndexer()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			list[1].Should().Be(42);
+		}
+
+		[Test]
+		public void InsertWorks()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			list.Insert(1, 99);
+			list.Count.Should().Be(4);
+			list[1].Should().Be(99);
+			list[2].Should().Be(42);
+		}
+
+		[Test]
+		public void ContainsWorksForList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			list.Contains(42).Should().BeTrue();
+		}
+
+		[Test]
+		public void ContainsFalseWorksForList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			list.Contains(420).Should().BeFalse();
+		}
+
+		[Test]
+		public void RemoveWorksForList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(7);
+			list.Add(978);
+			list.Add(35);
+			list.Add(42);
+
+			list.Remove(978).Should().BeTrue();
+			list.Count.Should().Be(4);
+		}
+
+		[Test]
+		public void RemoveAtWorksForList()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(7);
+			list.Add(978);
+			list.Add(35);
+			list.Add(42);
+
+			list.RemoveAt(2);
+			list.Count.Should().Be(4);
+			list[2].Should().Be(35);
+		}
+
+		[Test]
+		public void ListIndexOfTest()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			list.IndexOf(42).Should().Be(1);
+		}
+
+		[Test]
+		public void ClearRemovedAllItems()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+			list.Clear();
+
+			list.Should().NotBeNull();
+			list.Count.Should().Be(0);
+		}
+
+		[Test]
+		public void CopyToWorks()
+		{
+			var list = new CasList<int>();
+			list.Add(150);
+			list.Add(42);
+			list.Add(978);
+
+			var list2 = new int[3];
+
+			list.CopyTo(list2, 0);
+
+			list2.Length.Should().Be(3);
+			list2[0].Should().Be(150);
+			list2[1].Should().Be(42);
+			list2[2].Should().Be(978);
+		}
+	}
+}
